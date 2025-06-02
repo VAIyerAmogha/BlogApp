655d75cecb6413327577534ce6e58ca9a7d3a94d
Amogha V A <amogha.va.iyer@gmail.com>
2025-06-03 02:20:19 +0530

    created bloglist for the home page

--- blog-app/app/page.js
+++ blog-app/app/page.js
@@ -1,9 +1,12 @@
+"use client";
+import BlogList from "@/components/BlogList";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <BlogList />
     </>
   );
 }
