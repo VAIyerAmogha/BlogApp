35187e22d0268e276d38d1814f490134f8c9795d
Amogha V A <amogha.va.iyer@gmail.com>
2025-06-05 22:34:34 +0530

    completed the blog page

--- blog-app/app/blogs/[id]/page.jsx
+++ blog-app/app/blogs/[id]/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 import { assets, blog_data } from "@/assets/assets";
+import Footer from "@/components/Footer";
 import Image from "next/image";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 const Page = ({ params }) => {
@@ -21,20 +23,104 @@ const Page = ({ params }) => {
     fetchBlogData();
   }, []);
 
-  return (
-    <div className="bg-gray-200 py-5 px-5 md:px-12 lg:px-28">
-      <div className="flex justify-between items-center">
-        <Image
-          src={assets.logo}
-          width={180}
-          alt="logo"
-          className="w-[130px] sm:w-auto"
-        />
-        <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]">
-          Get started <Image src={assets.arrow} alt="logo" />
-        </button>
+  return data ? (
+    <>
+      <div className="bg-gray-200 py-5 px-5 md:px-12 lg:px-28">
+        <div className="flex justify-between items-center">
+          <Link href="/">
+            <Image
+              src={assets.logo}
+              width={180}
+              alt="logo"
+              className="w-[130px] sm:w-auto"
+            />
+          </Link>
+          <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]">
+            Get started <Image src={assets.arrow} alt="logo" />
+          </button>
+        </div>
+        <div className="text-center my-24">
+          <h1 className="text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto">
+            {data.title}
+          </h1>
+          <Image
+            src={data.author_img}
+            width={60}
+            height={60}
+            alt="img"
+            className="mx-auto mt-6 border border-white rounded-full"
+          />
+          <p className="mt-1 pb-2 text-lg max-w-[740px] mx-auto">
+            {data.author}
+          </p>
+        </div>
       </div>
-    </div>
+      <div className="mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10">
+        <Image
+          className="border-4 border-white"
+          src={data.image}
+          width={1280}
+          height={720}
+          alt="img"
+        />
+        <h1 className="my-8 text-[26px] font-semibold">Introduction</h1>
+        <p>{data.description}</p>
+        <h3 className="my-5 text-[18px] font-semibold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </h3>
+        <p className="my-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem modi
+          sequi dolorum ducimus qui obcaecati.
+        </p>
+        <p className="my-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem modi
+          sequi dolorum ducimus qui obcaecati.
+        </p>
+        <h3 className="my-5 text-[18px] font-semibold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </h3>
+        <p className="my-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem modi
+          sequi dolorum ducimus qui obcaecati.
+        </p>
+        <p className="my-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem modi
+          sequi dolorum ducimus qui obcaecati.
+        </p>
+        <h3 className="my-5 text-[18px] font-semibold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </h3>
+        <p className="my-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem modi
+          sequi dolorum ducimus qui obcaecati.
+        </p>
+        <p className="my-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem modi
+          sequi dolorum ducimus qui obcaecati.
+        </p>
+        <h3 className="my-5 text-[18px] font-semibold">Conclusion</h3>
+        <p className="my-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam tempore
+          iusto nostrum velit repellendus fuga culpa molestias distinctio
+          numquam at eius sapiente, nihil facilis maiores cum voluptatum ad
+          praesentium aut ducimus delectus nesciunt magnam. Labore pariatur
+          quisquam reiciendis recusandae facilis.
+        </p>
+        <div className="my-24">
+          <p className="text-black font font-semibold my-4">
+            Share this article on social media
+          </p>
+          <div className="flex">
+            <Image src={assets.facebook_icon} width={50} alt="img" />
+            <Image src={assets.twitter_icon} width={50} alt="img" />
+            <Image src={assets.googleplus_icon} width={50} alt="img" />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  ) : (
+    <></>
   );
 };
 
